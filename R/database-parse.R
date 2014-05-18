36815738b56ea0067a6843df029b60a07edc94dc
Hyunwoo Cho <hc27oclock@gmail.com>
2014-05-17 22:53:02 -0400

    irregularity finished

--- R/database-parse.R
+++ R/database-parse.R
@@ -1,5 +1,5 @@
 rm(list=ls())
-locs = read.csv("~/Downloads/StationEntrances.csv")
+locs = read.csv("~/Desktop/QBioSub/StationEntrances.csv")
 # we want a single letter for each line
 # Franklin shuttle (FS) is changed to small s
 # Grand Central shuttle (GS) is changed to large S
@@ -12,13 +12,30 @@ for (j in 6:16) {
 # sorts the lines and returns a collapsed vector
 locs$connected = apply(locs[, 6:16], 1, function(a) paste(sort(na.omit(gsub("\\s", "", a))), collapse=""))
 # correcting MTA's irregularitires
-locs$connected[c(160, 1214:1216, 1218)] = "DN"
-locs$connected[396] = "ACJLZ" # previously "ACJL"
-locs$connected[522] = "BDFM" # previously "7BDFM"
+# 4th Av-9th St
 locs$connected[552:553] = "FGR" # previously "F"
-locs$connected[785] = "23ACE" # previously "123ACE"
-locs$connected[1467] = "EFMR" # previously "eFMR"
+# Parkside Av, Neck Rd
+locs$connected[c(98, 108, 1198)] = "Q" # previously "BQ"
+# Franklin Av - Cs
+locs$connected[c(265, 1348)] = "Cs" # previously "As" or "ACs"
+# Liberty Av
+locs$connected[397:400] = "C" # previously "AC"
+# 168th St - Washington Heights
 locs$connected[c(285, 1279:1281)] = "1AC" # previously "AC"
+# Broadway Junction-East New York
+locs$connected[396] = "ACJLZ" # previously "ACJL"
+# 47-50th Sts Rockefeller Center
+locs$connected[522] = "BDFM" # previously "7BDFM"
+# Smith-9th St
+locs$connected[551] = "FG" # previously "FGR"
+# 7th Av, Prospect Park-15 St, Fort Hamilton Parkway, Church Av
+locs$connected[c(554:570, 1433:1439)] = "FG" # previously "F"
+# Long Island City-Court Square
+locs$connected[c(680, 1848:1849)] = "7EGM" # previously "G"
+# Park Place
+locs$connected[785] = "23ACE" # previously "123ACE"
+# Forest Hills-71st Av
+locs$connected[1467] = "EFMR" # previously "eFMR"
 
 locs$key = ""
 # rule
@@ -26,15 +43,16 @@ locs$key = ""
 # else: station name plus line connectivity
 # exception
 # 2- or 3-line crossing station with different names
-# DN (New Utrecht Av and 62nd St)
-# 6EM (Lexington Av-53rd St and 51st St)
-# FG (9th St and Smith-9th St)
-# FGR (9th St and Smith-9th St) 4 av
+# 1R (South Ferry and Whitehall St)
+# DN (62 St and New Utrecht Av)
+# GL (Metropolitan Av and Lorimer St)
 # 1AC (168th St - Washington Heights and 168th St)
+# 6EM (Lexington Av-53rd St and 51st St)
+# FGR (4 Av and 9th St)
 # 4-line crossing station but needs disambiguation
 # ABCD (125th St and 145th St)
 for (j in 1:nrow(locs)) {
-  if (nchar(locs$connected[j]) >= 4 | locs$connected[j] == "DN" | locs$connected[j] == "6EM" | locs$connected[j] == "FGR" | locs$connected[j] == "1AC") {
+  if (nchar(locs$connected[j]) >= 4 | locs$connected[j] == "1R" | locs$connected[j] == "DN" | locs$connected[j] == "GL" | locs$connected[j] == "1AC" | locs$connected[j] == "6EM" | locs$connected[j] == "FGR") {
     locs$key[j] = locs$connected[j]
   } else {
     locs$key[j] = paste(locs$Station_Name[j], locs$connected[j], sep="-")
@@ -46,29 +64,3 @@ for (j in 1:nrow(locs)) {
 }
 
 locs.coord = aggregate(cbind(Station_Latitude, Station_Longitude) ~ key, data=locs, function(a) mean(a))
-
-
-
-
-abcd.junctions = locs[locs$connected == "ABCD", c(1:5, 31)]
-large.junctions = locs[nchar(locs$connected) >= 4, c(1:5, 31)]
-large.junctions[order(large.junctions$connected), ]
-three.junctions = locs[nchar(locs$connected) == 3, c(1:5, 31)]
-three.junctions[order(three.junctions$connected), ]
-two.junctions = locs[nchar(locs$connected) == 2, c(1:5, 31)]
-two.junctions[order(two.junctions$connected), ]
-one.junctions = locs[nchar(locs$connected) == 1, c(1:5, 31)]
-one.junctions[order(one.junctions$connected), ]
-
-
-#37, Union Square => 14th St-Union Square
-#53-56, Lawrence St => Jay St - MetroTech
-#551, Smith-9thSt => 9th St
-"Lexington Av-53rd St" => ""
-
-
-#522, "7BDFM" => "BDFM"
-#1467, "eFMR" => "EFMR"
-#785, "123ACE" => "23ACE"
-
-#396, ACJL => ACJLZ
