623c7761ed2e5475d3dc1f1d39ea67bd6895b6c6
Hyunwoo Cho <hc27oclock@gmail.com>
2014-05-19 20:34:43 -0400

    added a new method nyc

--- python/systems.py
+++ python/systems.py
@@ -110,3 +110,45 @@ def labcdefg():
     
 
     return subway,lines,stations,trains
+
+def nyc():
+    subway = nx.Graph()
+
+    STATIONS = []
+    with open('../listOfStationsConverted.txt') as f:
+        no_using_header = f.readline()
+        rows = (line.strip().split("\t") for line in f)
+        for row in rows:
+            STATIONS.append(tuple([row[0], (int(row[5]) if (row[3][-2] == 'X') else int(row[5]) - 100000), (int(row[4]) if (row[3][-1] == 'L') else int(row[4]) - 100000), 0, 1]))
+
+    stations = list()
+
+    for S in STATIONS:
+        stations.append(objects.LineStation(*S))
+    #
+    for s in stations:
+        subway.add_node(s)
+
+    lines = { '1': objects.Line('1',stations[0:4]),
+              '2': objects.Line('2', [stations[4],stations[5],stations[2],stations[6]] )}
+
+    for l in lines.values():
+        for pair in pairwise(l.route):
+            dist = np.linalg.norm([pair[0].xy,pair[1].xy])
+            subway.add_edge(*pair, distance=dist)
+
+
+    train1 = objects.Train('1-1', 30, lines['1'], stations[0], 1, 1.0, verbose=0)
+    train2 = objects.Train('1-2', 30, lines['1'], stations[1], 1, 1.0, verbose=0)
+    train3 = objects.Train('1-3', 30, lines['1'], stations[2], 1, 1.0, verbose=0)
+    train4 = objects.Train('1-4', 30, lines['1'], stations[3], 1, 1.0, verbose=1)
+    train5 = objects.Train('1-1', 30, lines['1'], stations[0], -1, 1.0, verbose=0)
+    train6 = objects.Train('1-2', 30, lines['1'], stations[1], -1, 1.0, verbose=0)
+    train7 = objects.Train('1-3', 30, lines['1'], stations[2], -1, 1.0, verbose=0)
+    train8 = objects.Train('1-4', 30, lines['1'], stations[3], -1, 1.0, verbose=0)
+
+    trains = [train1,train2,train3,train4,train5,train6,train7,train8]
+    
+    
+
+    return subway,lines,stations,trains
